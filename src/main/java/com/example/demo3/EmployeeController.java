e89e269d6651556a50ed5550a2de6e3fbd15ca99
ajith <ajithsridharan.sridharan@gmail.com>
2025-04-02 15:53:30 +0530

    commit 4a

--- src/main/java/com/example/demo3/EmployeeController.java
+++ src/main/java/com/example/demo3/EmployeeController.java
@@ -1,7 +1,15 @@
 package com.example.demo3;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -10,12 +18,41 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/employee")
 public class EmployeeController {
 
+	@Autowired
 	EmployeeRepository repo;
 	
-	@GetMapping
+	@GetMapping("test")
 	public String getMessage()
 	{
 		return "Welcome to Spring boot";
 	}
+	
+	@PostMapping
+	public Employee addEmployee(@RequestBody Employee emp)
+	{
+		return repo.save(emp);
+	}
+	
+	@GetMapping
+	public List<Employee> getEmployees()
+	{
+		return repo.findAll();
+	}
+	
+	
+	@PutMapping("/$id")
+	public Employee updateEmployee(@RequestParam int id, @RequestBody Employee emp)
+	{
+		emp.setId(id);		
+		return repo.save(emp);
+		
+	}
+	
+	@DeleteMapping("/$id")
+	public void deleteEmployee(@RequestParam int id)
+	{
+		repo.deleteById(id);
+	}
+	
 	
 }
