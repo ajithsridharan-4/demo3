ce66b87027c091d0fa6aab219498e4b2bea773d3
ajith <ajithsridharan.sridharan@gmail.com>
2025-04-03 17:57:53 +0530

    commit 234234

--- src/main/java/com/example/demo3/EmployeeController.java
+++ src/main/java/com/example/demo3/EmployeeController.java
@@ -3,6 +3,7 @@ package com.example.demo3;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/employee")
+@CrossOrigin("*")
 public class EmployeeController {
 
 	@Autowired
